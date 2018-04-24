de91a3fccfedf04a8b881de13c7d460c6bd9c60c
Zixuan Chen <zixuan3@illinois.edu>
2018-04-24 12:32:24 -0500

    lab

--- app/src/main/java/com/example/zixua/project1/MainActivity.java
+++ app/src/main/java/com/example/zixua/project1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.zixua.project1;
 
+import java.util.Random;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,66 +25,11 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-//public class MainActivity extends AppCompatActivity {
-//    private static final String TAG = "FinalProject:Main";
-//    /**
-//     * dfdfdfdfdf.
-//     */
-////    private static final String STR = "faa727bab1930c706d60e0e7abfb625a";
-//    private static final String STR = " b016ae9dde8191e7272cf40829a7f841";
-//    /** Request queue for our API requests. */
-//    private static RequestQueue requestQueue;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        final ImageButton startProject = findViewById(R.id.imageButton);
-//        startProject.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d(TAG, "imageButtonClicked");
-//                startAPICall();
-//            }
-//        });
-//        setContentView(R.layout.activity_main);
-//    }
-//    /**
-//     * Make a call to the weather API.
-//     */
-//    void startAPICall() {
-//        try {
-//            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-//                    Request.Method.GET,
-//                    "https://yesno.wtf/api",
-//                    null,
-//                    new Response.Listener<JSONObject>() {
-//                        @Override
-//                        public void onResponse(final JSONObject response) {
-//                            try {
-//                                Log.d(TAG, response.toString(2));
-//                                TextView tv = (TextView) findViewById(R.id.textView);
-//                                tv.setText(response.toString(2));
-//                            } catch (JSONException ignored) { }
-//                        }
-//                    }, new Response.ErrorListener() {
-//                @Override
-//                public void onErrorResponse(final VolleyError error) {
-//                    Log.e(TAG, error.toString());
-//                }
-//            });
-//            requestQueue.add(jsonObjectRequest);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+
 public final class MainActivity extends AppCompatActivity {
     /** Default logging tag for messages from the main activity. */
     private static final String TAG = "Lab12:Main";
-    /**
-     * dfdfdfdfdf.
-     */
-//    private static final String STR = "faa727bab1930c706d60e0e7abfb625a";
-    private static final String STR = " b016ae9dde8191e7272cf40829a7f841";
+
     /** Request queue for our API requests. */
     private static RequestQueue requestQueue;
 
@@ -111,6 +57,26 @@ public final class MainActivity extends AppCompatActivity {
                 startAPICall();
             }
         });
+        final Button cat = findViewById(R.id.button2);
+        cat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "button clicked");
+//                Toast.makeText(getApplicationContext(), "Problem taking photo",
+//                        Toast.LENGTH_LONG).show();
+                anotherAPI();
+            }
+        });
+        final Button joke = findViewById(R.id.button3);
+        joke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "button clicked");
+//                Toast.makeText(getApplicationContext(), "Problem taking photo",
+//                        Toast.LENGTH_LONG).show();
+                thirdAPI();
+            }
+        });
     }
 
     /**
@@ -135,10 +101,10 @@ public final class MainActivity extends AppCompatActivity {
                         public void onResponse(final JSONObject response) {
                             try {
                                 Log.d(TAG, response.toString(2));
-//                                TextView tv = (TextView) findViewById(R.id.textView);
-//                                tv.setText(response.toString(2));
-//                                tv.setText("text------");
+                                TextView tv = (TextView) findViewById(R.id.textView);
+                                tv.setVisibility(View.GONE);
                                 WebView wv = (WebView) findViewById(R.id.webview);
+                                wv.setVisibility(View.VISIBLE);
                                 wv.loadUrl(parseJson(response));
                             } catch (JSONException ignored) { }
                         }
@@ -153,6 +119,45 @@ public final class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+    void thirdAPI() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            try {
+                                Log.d(TAG, response.toString(2));
+                                TextView tv = (TextView) findViewById(R.id.textView);
+                                tv.setVisibility(View.VISIBLE);
+                                tv.setText(parseJoke(response));
+                                WebView wv = (WebView) findViewById(R.id.webview);
+                                wv.setVisibility(View.GONE);
+                            } catch (JSONException ignored) { }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    void anotherAPI() {
+        WebView wv = (WebView) findViewById(R.id.webview);
+        Random rand = new Random();
+        int randomW = rand.nextInt(216) + 200;
+        int randomH = rand.nextInt(211) + 300;
+        wv.setVisibility(View.VISIBLE);
+        wv.loadUrl("https://placekitten.com/" + randomW + "/" + randomH);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setVisibility(View.GONE);
+    }
     public static String parseJson(JSONObject response) {
         String str = response.toString();
         JsonParser parser = new JsonParser();
@@ -161,5 +166,14 @@ public final class MainActivity extends AppCompatActivity {
 //        String forced = ret.get("forced").getAsString();
         String image = ret.get("image").getAsString();
         return image;
+    }
+    public static String parseJoke(JSONObject response) {
+        String str = response.toString();
+        JsonParser parser = new JsonParser();
+        JsonObject ret = parser.parse(str).getAsJsonObject();
+        String setup = ret.get("setup").getAsString();
+        String punchline = ret.get("punchline").getAsString();
+        str = setup + "\n" + "\n" + punchline;
+        return str;
     }
 }
